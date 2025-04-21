5cfce312c5ac65a47e7c93ab89b64acafe2b5e99
DAMIEN FONTAINE <zelia.kowalski@gmail.com>
2025-04-21 17:52:30 +0200

    Ajout de la page About + corrections sur App

---
+++ client/src/pages/About.tsx
@@ -0,0 +1,32 @@
+import "../styles/About.scss";
+
+export const About = () => {
+  return (
+    <div className="about-container">
+        <h2>ABOUT</h2>
+        <div className="underline"></div>
+      <p>
+      Welcome to Le Chineur , a music platform designed for everyone who can't go a day without listening to their favorite tunes .
+</p>
+
+<h4>THE IDEA BEHIND LE CHINEUR</h4>
+<p>
+It all started with a simple question :
+“Why is it sometimes so hard to find the right playlist for a specific vibe ?”
+We love Spotify , but with millions of tracks and auto-generated playlists that don't always hit the mark , we wanted to offer something more personal , more human , more cool .
+So we created Le Chineur , a web app that uses the Spotify API to deliver carefully curated playlists .
+Whether you're coding , working out , daydreaming , or crying in the dark (hey, it happens), you'll find the perfect sound atmosphere just for you .
+</p>
+
+<h4>WHO ARE WE ?</h4>
+<p>
+We're a small team of web development students , passionate about music , technology , and user experience .
+This project was born in an educational setting , but we've poured our energy , creativity , and love for great sound into it .
+</p>
+
+      <p>
+      You ? Maybe one day you'll join our team or become part of our community!
+      </p>
+    </div>
+  );
+};
