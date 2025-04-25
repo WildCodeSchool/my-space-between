7e18df1036baff7c0c170b783ad95100ee77b365
Adrien Meert <adrien.meert92@gmail.com>
2025-04-25 10:34:29 +0200

    Spotify-related login button

--- client/src/components/SpotifyLoginButton.tsx
+++ client/src/components/SpotifyLoginButton.tsx
@@ -4,10 +4,23 @@ type Props = {
     onClick: () => void;
 };
 
+const SCOPE = import.meta.env.VITE_SCOPE;
+const AUTH_ENDPOINT = import.meta.env.VITE_AUTH_ENDPOINT;
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+
 const SpotifyLoginButton = ({ onClick }: Props) => {
+    const spotifyLogin = () => {
+        const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+            REDIRECT_URI
+        )}&response_type=code&scope=${encodeURIComponent(SCOPE)}`;
+
+        console.log("URL de redirection Spotify :", authUrl);
+        window.location.href = authUrl;
+    };
     return (
         <button
-            onClick={onClick}
+            onClick={spotifyLogin}
             className="loginButton"
             title="login in Spotify"
         >
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -20,10 +20,13 @@ import App from "./App";
 // You can add more routes as you build out your app!
 const router = createBrowserRouter([
   {
-    path: "/", // The root path
-    element: <App />, // Renders the App component for the home page
+    path: "/",
+    element: <App />,
   },
-  // Try adding a new route! For example, "/about" with an About component
+  {
+    path: "/callback",
+    element: <App />,
+  }
 ]);
 
 /* ************************************************************************* */
@@ -34,13 +37,11 @@ if (rootElement == null) {
   throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
 
-// Render the app inside the root element
 createRoot(rootElement).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 );
-
 /**
  * Helpful Notes:
  *
