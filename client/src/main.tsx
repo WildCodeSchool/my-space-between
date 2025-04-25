6340486a60ff9e9dac036f13e19a2f98aae2595d
DAMIEN FONTAINE <zelia.kowalski@gmail.com>
2025-04-25 16:49:57 +0200

    "Corrected css and about"

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,72 +1,30 @@
-//Import necessary modules from React and React Router
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
-
-/* ************************************************************************* */
-
-// Import the main app component
 import App from "./App";
+import { About } from "./pages/About";
 
-// Import additional components for new routes
-// Try creating these components in the "pages" folder
-
-import { FetchDataProvider } from "./context/FetchDataContext";
-import { MusicProvider } from "./context/MusicContext"; 
-
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-import Home from "./pages/Home"; // 
-import Player from "./pages/Player"; // 
-
-/* ************************************************************************* */
-
-// Create router configuration with routes
-// You can add more routes as you build out your app!
 const router = createBrowserRouter([
   {
-    element: <App/>,
-    children: [
-      {
-        path: "/", // The root path
-        element: <Home />,
-       }, // Renders the App component for the home page
-      { path: "/player", 
-        element: <Player /> 
-      },
-    ]
-  }
-   // Renders the App component for the home page
-  // Try adding a new route! For example, "/about" with an About component
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
 ]);
 
-/* ************************************************************************* */
-
-// Find the root element in the HTML document
 const rootElement = document.getElementById("root");
-if (rootElement == null) {
-  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+if (!rootElement) {
+  throw new Error("Your HTML document should contain a <div id=\"root\"></div>");
 }
 
-// Render the app inside the root element
 createRoot(rootElement).render(
   <StrictMode>
-    <FetchDataProvider>
-      <MusicProvider>
-        <RouterProvider router={router} />
-      </MusicProvider>
-    </FetchDataProvider>
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 );
-
-/**
- * Helpful Notes:
- *
- * 1. Adding More Routes:
- *    To add more pages to your app, first create a new component (e.g., About.tsx).
- *    Then, import that component above like this:
- *
- *    import About from "./pages/About";
 
 
 
