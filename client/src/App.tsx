526364db622e87c9aab83b7b35b69f9801f07618
Salvak613 <jules.clauwaert@gmail.com>
2025-04-25 14:01:12 +0200

    merged files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,15 @@
 import "./App.css";
 
 import { Outlet } from "react-router-dom";
-
-
+import BurgerMenu from "./components/BurgerMenu";
 
 function App() {
   return (
     <>
+      <BurgerMenu />
       <Outlet />
     </>
   );
 }
 
-
 export default App;
-
-
