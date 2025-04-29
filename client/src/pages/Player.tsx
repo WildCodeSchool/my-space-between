d9fb2820511d7c4aa9d8b9e9a3a21ecbd6b5dcda
Jerome Marbach <jeromemarbach@outlook.fr>
2025-04-29 15:07:59 +0200

    commit for save

--- client/src/pages/Player.tsx
+++ client/src/pages/Player.tsx
@@ -1,39 +1,56 @@
 import SpotifyPlayer from "../components/SpotifyPlayer";
 import { useFetchDataContext } from "../context/FetchDataContext";
+import styles from "./Player.module.css";
 
 const Player = () => {
   const { musicList } = useFetchDataContext();
 
   return (
-    <div>
+    <div className={styles.GlobalPlayer}>
       {musicList.length > 0 ? (
         musicList.map((item) => (
           <div key={item.id}>
-            <p>ID: {item.id}</p>
-            <p>Popularity: {item.popularity}</p>
-            <p>
-              URL: <SpotifyPlayer uri={item.url} />
-              <a href={item.url} target="_blank" rel="noopener noreferrer">
-                {item.url}
-              </a>
+            <p className={styles.playerId}>ID: {item.id}</p>
+            <p className={styles.windowPlayer}>
+              <SpotifyPlayer uri={item.url} />
+              <a href={item.url} target="_blank" rel="noopener noreferrer"></a>
             </p>
-            <p>Name: {item.name}</p>
-            <p>Artist: {item.artist}</p>
-            {item.image && <img src={item.image} alt={item.name} />}
-            <p>Genre: {item.genre}</p>
-            <p>Followers: {item.followers}</p>
-            {item.artistImage && (
-              <img src={item.artistImage} alt={`${item.artist}'s image`} />
-            )}
-            <div>
-              <h3>Albums:</h3>
-              {item.albums.map((album, index) => (
-                <div key={index}>
-                  <p>Album Name: {album.name}</p>
-                  {album.image && <img src={album.image} alt={album.name} />}
-                </div>
-              ))}
-            </div>
+            <section className={styles.sectionInfoPlayer}>
+              {item.artistImage && (
+                <img
+                  className={styles.pictureArtisteProfil}
+                  src={item.artistImage}
+                  alt={`${item.artist}'s image`}
+                />
+              )}
+              <p>Artist: {item.artist}</p>
+              <p>Name: {item.name}</p>
+              <p>Followers: {item.followers}</p>
+              <p>Genre: {item.genre}</p>
+              <p>Popularity: {item.popularity}</p>
+              {item.image && (
+                <img
+                  className={styles.pictureInPlayer}
+                  src={item.image}
+                  alt={item.name}
+                />
+              )}
+              <div className={styles.listAlbumPlayer}>
+                <h3>Albums:</h3>
+                {item.albums.map((album, index) => (
+                  <div key={index}>
+                    <p>Album Name: {album.name}</p>
+                    {album.image && (
+                      <img
+                        className={styles.pictureAlbumPlayer}
+                        src={album.image}
+                        alt={album.name}
+                      />
+                    )}
+                  </div>
+                ))}
+              </div>
+            </section>
           </div>
         ))
       ) : (
