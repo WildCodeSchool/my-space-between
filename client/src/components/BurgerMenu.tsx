521634e3aeb0b240eb4651ea142db17055123c1c
Adrien Meert <adrien.meert92@gmail.com>
2025-05-15 14:19:14 +0200

    Adding an event to the burger menu

--- client/src/components/BurgerMenu.tsx
+++ client/src/components/BurgerMenu.tsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./BurgerMenu.module.css";
 import { Link } from "react-router-dom";
 
 function BurgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <div className={styles.burgerMenuWrapper}>
+    <div className={styles.burgerMenuWrapper} ref={menuRef}>
       <button
         onClick={toggleMenu}
         className={`${styles.menuBurger} ${isOpen ? styles.open : ""}`}
@@ -18,15 +35,9 @@ function BurgerMenu() {
 
       <nav className={`${styles.navBar} ${isOpen ? styles.open : ""}`}>
         <ul className={styles.menuOpen}>
-          <li>
-            <Link to="/">HOME</Link>
-          </li>
-          <li>
-            <Link to="/about">ABOUT</Link>
-          </li>
-          <li>
-            <Link to="/contact">CONTACT</Link>
-          </li>
+          <li><Link to="/">HOME</Link></li>
+          <li><Link to="/about">ABOUT</Link></li>
+          <li><Link to="/contact">CONTACT</Link></li>
         </ul>
       </nav>
     </div>
